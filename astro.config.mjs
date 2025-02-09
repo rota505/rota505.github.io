e682d279fbc98e7217ffd960867ac7698ba041e6
Rota <sryota461@gmail.com>
2025-02-09 15:56:29 +0900

    add md layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-base: 'rota505.github.io',
+site: 'https://rota505.github.io',
 output: 'static',
 vite: {
     plugins: [tailwindcss()],
