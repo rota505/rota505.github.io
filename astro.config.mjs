f73848e00a2cc874bc3f864b052616b3ad81ddf8
Rota <sryota461@gmail.com>
2025-02-14 18:16:54 +0900

    wip

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-site: 'https://rota505.github.io/',
+site: 'https://rota505.github.io',
 vite: {
     plugins: [tailwindcss()],
   },
